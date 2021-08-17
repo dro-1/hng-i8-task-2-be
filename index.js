c630545db898a132c2ff9811418c05c697ce4384
Seun Taiwo <lorddro1532@gmail.com>
2021-08-17 18:55:40 +0100

    Project Init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/books", bookRouter);
+
+app.listen(PORT, () => {
+  debug(`Listening on port ${chalk.green(PORT)}`);
+});
